1d741ca39530c9668fbbdcd2a7ee3d4104d314dd
xeneskus <xeneskus@gmail.com>
2023-11-04 23:41:49 +0300

    blog details page updated

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace DataAccessLayer.Abstract
         void Delete(T t);
         void Update(T t);
         List<T> GetListAll();
-        T GetById(int id); 
+        T GetById(int id);
+        List<T> GetListAll(Expression<Func<T, bool>> filter);
     }
 }
