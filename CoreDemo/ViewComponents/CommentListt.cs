6a4ac64f8c9141a2d83d0bf87351df633838d00d
xeneskus <xeneskus@gmail.com>
2023-11-06 10:56:21 +0300

    Listing Comments on Blogs

---
+++ CoreDemo/ViewComponents/CommentListt.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentListt : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
