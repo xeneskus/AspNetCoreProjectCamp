419e2bf4d14340defc8aa81c17e1080e9f7794d3
xeneskus <xeneskus@gmail.com>
2023-11-07 12:29:26 +0300

    added contact page

---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContactDate = DateTime.Parse(DateTime.Now.ToLongDateString());
+            p.ContactStatus = true;
+            cm.ContactAdd(p);
+            return RedirectToAction("Index", "Blog"); ;
+        }
+    }
+}
