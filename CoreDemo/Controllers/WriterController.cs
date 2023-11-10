1734e4540713d9f638706de190f05737d6d5a5ae
xeneskus <xeneskus@gmail.com>
2023-11-10 14:21:28 +0300

    Author template uploaded. Added Author Top Menu View Components

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -19,5 +19,10 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult Test() 
+        {
+            return View();
+        }
     }
 }
