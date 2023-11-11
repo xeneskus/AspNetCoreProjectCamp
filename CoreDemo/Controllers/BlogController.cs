3fa83e0084a646968cfecba7ed48af1f00015798
xeneskus <xeneskus@gmail.com>
2023-11-11 18:11:01 +0300

    Added Author's Own Blog List

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -20,6 +20,11 @@ namespace CoreDemo.Controllers
             var values = bm.GetBlogById(id);
             return View(values);
         }
+        public IActionResult BlogListByWriter()
+        {
+           var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
 
     }
 }
