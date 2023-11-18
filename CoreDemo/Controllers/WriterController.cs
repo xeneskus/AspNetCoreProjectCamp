17914e3ba345210fc1ce65506808b07e16c25f8b
xeneskus <xeneskus@gmail.com>
2023-11-18 19:24:59 +0300

    Added Upload Profile Photo from File

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using CoreDemo.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace CoreDemo.Controllers
 {
@@ -65,6 +67,35 @@ namespace CoreDemo.Controllers
                 }
             }
             return View();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            
+            return View();
+        }   
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+            Writer writer = new Writer();
+            if (p.WriterImage != null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/",newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                writer.WriterImage = newImageName;
+            }
+            writer.WriterMail = p.WriterMail;
+            writer.WriterName = p.WriterName;
+            writer.WriterPassword = p.WriterPassword;
+            writer.WriterStatus = true;
+            writer.WriterAbout = p.WriterAbout;
+            writerManager.TAdd(writer);
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
