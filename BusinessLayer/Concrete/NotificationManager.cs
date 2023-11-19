7d57f141a4af5f5daa34514f30f009e71dd938da
xeneskus <xeneskus@gmail.com>
2023-11-19 21:56:16 +0300

    Added listing of notifications

--- BusinessLayer/Concrete/NotificationManager.cs
+++ BusinessLayer/Concrete/NotificationManager.cs
@@ -15,7 +15,7 @@ namespace BusinessLayer.Concrete
 
         public List<Notification> GetList()
         {
-            throw new NotImplementedException();
+            return _notificationDal.GetListAll();
         }
 
         public void TAdd(Notification t)
