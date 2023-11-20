c2c45add32a7feead94d3c24ed5d132f9cdd46fd
xeneskus <xeneskus@gmail.com>
2023-11-20 10:30:05 +0300

    Added Message Notifications

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterMessageNotification : ViewComponent
     {
+        MessageManager messageManager = new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p;
+            p = "enes@gmail.com";
+            var values = messageManager.GetInboxListByWriter(p);
+            return View(values);
         }
     }
 }
