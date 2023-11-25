e0f7861489e5925dc777cc666b724e6ec06f3fae
xeneskus <xeneskus@gmail.com>
2023-11-25 22:41:47 +0300

    Areas Map Controller Route

---
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Admin")] //areadan geldiğini bildirdik
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CoreDemo/Program.cs
+++ CoreDemo/Program.cs
@@ -49,7 +49,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
