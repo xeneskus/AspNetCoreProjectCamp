b68b8fec56c75dd23364a31de78f4de89ec3d1eb
xeneskus <xeneskus@gmail.com>
2023-11-26 20:35:55 +0300

    added Pagination

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,16 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
-
+using X.PagedList;
 namespace CoreDemo.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
         [Area("Admin")] //areadan geldiğini bildirdik
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var values = categoryManager.GetList();
+            var values = categoryManager.GetList().ToPagedList(page,3);
             return View(values);
         }
     }
