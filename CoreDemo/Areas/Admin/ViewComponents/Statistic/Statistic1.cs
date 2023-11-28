6ce9c01510b91066f087c605e0e0839264c6d492
xeneskus <xeneskus@gmail.com>
2023-11-28 08:34:48 +0300

    Pulling Weather Information via Api

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = blogManager.GetList().Count();
             ViewBag.v2 = context.Contacts.Count();
             ViewBag.v3 = context.Comments.Count();
+
+            string api = "47ef9ad4bb11c9fcb60c7fabd8b3985a";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value; //sıcaklıgı çektik
             return View();
         }
     }
