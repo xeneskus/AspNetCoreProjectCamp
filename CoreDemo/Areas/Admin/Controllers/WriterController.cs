5da7b526ee585120211596b258137054f6e18f0f
xeneskus <xeneskus@gmail.com>
2023-11-28 09:36:32 +0300

    Consoleda Listing of Data with Ajax

---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,39 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Enes"
+            },
+            new WriterClass
+            {
+                Id=2,
+                Name="Ahmet"
+            },
+            new WriterClass
+            {
+                Id=3,
+                Name="Selim"
+            },
+
+        };
+    }
+}
