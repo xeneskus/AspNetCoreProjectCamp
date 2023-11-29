f5bf551266fd576cc3924d9ef1b5c95e1252d4e2
xeneskus <xeneskus@gmail.com>
2023-11-29 22:44:42 +0300

    api layer created. entity added

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using BlogApiDemo.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=NIRVANA\\SQLEXPRESS05;database=CoreBlogApiDB;integrated security=true;TrustServerCertificate=True; "); //sql e bagladık
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
