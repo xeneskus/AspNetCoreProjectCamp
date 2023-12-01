9105cbc82ec6a7a21fcb794d892a5cd88767bc7a
xeneskus <xeneskus@gmail.com>
2023-12-01 22:35:51 +0300

    Project Communication with Apis. List & Add

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:7011/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            StringContent stringContent = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:7011/api/Default", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+    }
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
