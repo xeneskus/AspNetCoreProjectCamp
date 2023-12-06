62bdfe76fa9ab6c5234d33c367704da2a5edc24f
xeneskus <xeneskus@gmail.com>
2023-12-06 18:40:17 +0300

    Retrieving the Information of the User Logged in with Identity

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Writer
@@ -8,12 +10,16 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterAboutOnDashboard : ViewComponent
     {
         WriterManager writerManager = new WriterManager(new EfWriterRepository());
-        
+
+
+
         Context c = new Context(); 
-        public IViewComponentResult Invoke()
+        public  IViewComponentResult Invoke()
         {
-            var userMail = User.Identity.Name;
-            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var userName = User.Identity.Name;
+            ViewBag.veri = userName;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userName).Select(y => y.WriterID).FirstOrDefault();
             var values = writerManager.GetWriterById(writerID);
             return View(values);
         }
